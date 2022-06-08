e45234a50c0d13439e4ff7aea55a93131475e533
ShoichiroShirakawa <76085775+ShoichiroShirakawa@users.noreply.github.com>
2022-06-08 10:47:33 +0900

    Add files via upload

---
+++ 03/dist/js/about.06d4df98.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksite"]=self["webpackChunksite"]||[]).push([[443],{2209:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],a=n(1001),l={},r=(0,a.Z)(l,s,u,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.06d4df98.js.map
